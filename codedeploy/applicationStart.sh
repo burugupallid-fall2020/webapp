a4b973dadabd05fe5ffebe542d58b22070113369
dburugupalli <burugupalli.d@northeastern.edu>
2020-11-03 17:17:43 -0500

    codedeploy-scripts

--- codedeploy/applicationStart.sh
+++ codedeploy/applicationStart.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo "Navigate to /home/ubuntu/webapp=========="
+echo "------Start of Application Start-------"
+TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+echo "$TIMESTAMP"
+
+echo "Navigate to /home/ubuntu/webapp"
 cd /home/ubuntu/webapp
 echo "start the webapplication and store the logs to app.log"
 sudo nohup node app.js >> app.log 2>&1 &
+
+TIMESTAMP=`date "+%Y-%m-%d %H:%M:%S"`
+echo "$TIMESTAMP"
+echo "------End of Application Start-------"
